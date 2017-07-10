af76292b037f3304bd6cc664d57445e560d94424
Reza Roohian <m.roohian87@gmail.com>
2017-07-11 00:25:32 +0200

    user model, data service and loading implemented.

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data/data.service';
+import { User } from '../../models/user.model'
 
 @Component({
   selector: 'app-content',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
+  users: User[];
 
-  constructor() { }
+  constructor(public dataService: DataService) { }
 
   ngOnInit() {
   }
 
+  loadUsers() {
+    console.log('loadUsers');
+    this.users = this.dataService.getUsers();
+  }
 }
--- src/app/services/data/data.service.ts
+++ src/app/services/data/data.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { User } from '../../models/user.model'
 
 @Injectable()
 export class DataService {
-
   constructor() { }
 
+  getUsers() : User[] {
+    return [{
+      userId: 1,
+      name: 'afsoun amiri',
+      email: 'afsoun1981@gmail.com'
+    }, {
+      userId: 2,
+      name: 'reza roohian',
+      email: 'mrroohian@gmail.com'
+    }];
+  }
 }
